b3c0f3902a3a4058cbddbe0d4e622f14d37b8c9e
atan241200 <atan241200@gmail.com>
2025-02-24 00:06:43 +0700

    update Ui FielInfor and FieldSchedule

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import CardList from "@/components/CardList";
+import FieldInfor from "@/components/FieldInfor";
+import FieldSchedule from "@/components/FieldSchedule";
+
+
 import React from 'react'
 export default function page() {
   return (
     <div>
-       <CardList />
+       {/* <CardList /> */}
+       <FieldInfor/>
+       <FieldSchedule/>
     </div>
   )
 }
