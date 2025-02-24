f56dbdae30aaa8ad0e1e408dcf3735b4637e5a07
atan241200 <atan241200@gmail.com>
2025-02-24 08:51:04 +0700

    remove file confix

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 import CardList from "@/components/CardList";
-import FieldInfor from "@/components/FieldInfor";
-import FieldSchedule from "@/components/FieldSchedule";
 
 
 import React from 'react'
 export default function page() {
   return (
     <div>
-       {/* <CardList /> */}
-       <FieldInfor/>
-       <FieldSchedule/>
+       <CardList/>
+
     </div>
   )
 }
