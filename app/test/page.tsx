f5c49f549afbcd284a9bdba3268d2e8622dd12db
atan241200 <atan241200@gmail.com>
2025-02-24 08:48:21 +0700

    update Ui FielDetail-FielSchedule

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,44 +1,14 @@
-"use client"
-import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-} from "@heroui/react";
+"use client";
+import FieldInfor from "@/components/FieldInfor";
+import FieldSchedule from "@/components/FieldSchedule";
 
-export default function App() {
+
+import React from 'react'
+export default function page() {
   return (
-    <Table aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn>NAME</TableColumn>
-        <TableColumn>ROLE</TableColumn>
-        <TableColumn>STATUS</TableColumn>
-      </TableHeader>
-      <TableBody>
-        <TableRow key="1">
-          <TableCell>Tony Reichert</TableCell>
-          <TableCell>CEO</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key="2">
-          <TableCell>Zoey Lang</TableCell>
-          <TableCell>Technical Lead</TableCell>
-          <TableCell>Paused</TableCell>
-        </TableRow>
-        <TableRow key="3">
-          <TableCell>Jane Fisher</TableCell>
-          <TableCell>Senior Developer</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key="4">
-          <TableCell>William Howard</TableCell>
-          <TableCell>Community Manager</TableCell>
-          <TableCell>Vacation</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
-  );
+    <div>
+       <FieldInfor/>
+       <FieldSchedule/>
+    </div>
+  )
 }
-
