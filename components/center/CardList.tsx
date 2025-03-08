4188d6c81f1f193d5020dc477370d86a8924c81d
quyenduc26 <quyenduc26@gmail.com>
2025-03-08 12:17:33 +0700

    s3_enhance-UI-and-integrate-search

--- components/center/CardList.tsx
+++ components/center/CardList.tsx
@@ -1,42 +1,25 @@
 "use client";
-import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
 
 import CardField from "./CardField";
 
-import playingApi from "@/service/playingApi";
 import { CardFieldType } from "@/types";
 
-const CardList = () => {
-  const [fields, setFields] = useState<CardFieldType[]>([]);
-  const searchParams = useSearchParams();
-
-  const fromTime = searchParams.get("fromTime");
-  const toTime = searchParams.get("toTime");
-
-  const fetchData = async () => {
-    try {
-      const response = await playingApi.getAllCenter();
-      const data = response.data.content;
-
-      setFields(data);
-    } catch (error) {
-      console.error("Lỗi khi lấy dữ liệu sân bóng:", error);
-      setFields([]);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+const CardList = ({
+  fields,
+  queryTime,
+}: {
+  fields: CardFieldType[];
+  queryTime?: { fromTime: string; toTime: string };
+}) => {
+  const fromTime = queryTime?.fromTime;
+  const toTime = queryTime?.toTime;
 
   return (
     <div className="container mx-auto p-4 mt-10 mb-10">
-      {/* <h1 className="text-6xl font-bold text-center mb-4">Danh sách sân bóng</h1> */}
       {fields.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {fields.map((field) =>
-            fromTime != null && toTime != null ? (
+            fromTime && toTime ? (
               <CardField
                 key={field.id}
                 field={field}
