04e8434d9f6819f169267c1ab0f5e3e9d72bba06
quyenduc26 <quyenduc26@gmail.com>
2025-03-11 15:24:41 +0700

    fix detail team type

--- components/team/inforTeam/detailTeam.tsx
+++ components/team/inforTeam/detailTeam.tsx
@@ -42,6 +42,7 @@ export default function TeamIntro() {
             name={team.name}
             sport={team.sport}
             teamId={team.id}
+            cover={team.cover}
           />
           <PlayerTable players={team.members || []} teamId={team.id} />
         </>
